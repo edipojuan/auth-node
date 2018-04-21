36401cc0a74682d59c18d67143ac7ccf1e25e721
Édipo Juan <edipojs@gmail.com>
2018-04-21 00:12:57 -0400

    Primeiro commit

---
+++ src/middlewares/auth.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth.json');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send({ erro: 'O token não foi informado' });
+  }
+
+  const parts = authHeader.split(' ');
+
+  if (!parts.length === 2) {
+    return res.status(401).send({ erro: 'O token não contém duas partes' });
+  }
+
+  const [scheme, token] = parts;
+
+  if (!/^Bearer$/i.test(scheme)) {
+    return res.status(401).send({ erro: 'O Token não está no formato correto' });
+  }
+
+  jwt.verify(token, authConfig.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ erro: 'Token inválido' });
+    }
+
+    req.userId = decoded.id;
+    return next();
+  });
+};
